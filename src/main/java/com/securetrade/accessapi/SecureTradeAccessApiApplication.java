a233042e1d40766a11730ba8dfeeee8ce34446bb
jithmapv <jithma.hpv@gmail.com>
2026-08-12 13:09:29 +0530

    chore: add initial Spring Boot project setup

---
+++ src/main/java/com/securetrade/accessapi/SecureTradeAccessApiApplication.java
@@ -0,0 +1,13 @@
+package com.securetrade.accessapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureTradeAccessApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureTradeAccessApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/securetrade/accessapi/SecureTradeAccessApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securetrade.accessapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureTradeAccessApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
