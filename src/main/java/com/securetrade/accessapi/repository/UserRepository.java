7115b2fece6e60f9e85d6a6d6b2f039ef9f6891d
jithmapv <jithma.hpv@gmail.com>
2026-08-23 12:11:31 +0530

    feat: create UserRepository interface for user data access

---
+++ src/main/java/com/securetrade/accessapi/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.securetrade.accessapi.repository;
+
+import com.securetrade.accessapi.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+
+    Optional<UserEntity> findByUsername(String username);
+
+    boolean existsByUsername(String username);
+}
