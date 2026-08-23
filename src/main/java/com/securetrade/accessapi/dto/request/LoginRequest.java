007eb60c950165ccda2b9ad8bc8f5181c550a92e
jithmapv <jithma.hpv@gmail.com>
2026-08-23 17:12:48 +0530

    feat: add OpenAPI annotations to LoginRequest for improved API documentation

--- src/main/java/com/securetrade/accessapi/dto/request/LoginRequest.java
+++ src/main/java/com/securetrade/accessapi/dto/request/LoginRequest.java
@@ -1,12 +1,16 @@
 package com.securetrade.accessapi.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
+@Schema(description = "Login credentials")
 public class LoginRequest {
 
+    @Schema(description = "Account username", example = "agent.one")
     @NotBlank(message = "Username is required")
     private String username;
 
+    @Schema(description = "Account password", example = "StrongPassword123!", format = "password")
     @NotBlank(message = "Password is required")
     private String password;
 
