a9d988c001ba1f9fad7063e0ebbfaae280b8c1c2
jithmapv <jithma.hpv@gmail.com>
2026-08-23 13:57:20 +0530

    feat: add EntityGraph annotation to findById method in AccessRequestRepository for optimized fetching

--- src/main/java/com/securetrade/accessapi/repository/AccessRequestRepository.java
+++ src/main/java/com/securetrade/accessapi/repository/AccessRequestRepository.java
@@ -3,12 +3,17 @@ package com.securetrade.accessapi.repository;
 import com.securetrade.accessapi.entity.AccessRequestEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface AccessRequestRepository extends JpaRepository<AccessRequestEntity, UUID> {
+
+    @Override
+    @EntityGraph(attributePaths = {"agent", "agent.user"})
+    Optional<AccessRequestEntity> findById(UUID id);
 
     Page<AccessRequestEntity> findByAgentId(UUID agentId, Pageable pageable);
 
