f1bc7f647054ef769be227a621d15609cc273518
jithmapv <jithma.hpv@gmail.com>
2026-08-23 11:25:52 +0530

    feat: add ErrorCode enum for common error handling

---
+++ src/main/java/com/securetrade/accessapi/common/enums/ErrorCode.java
@@ -0,0 +1,10 @@
+package com.securetrade.accessapi.common.enums;
+
+// Common error codes
+public enum ErrorCode {
+    RESOURCE_NOT_FOUND,
+    DUPLICATE_RESOURCE,
+    INVALID_REQUEST,
+    ACCESS_DENIED,
+    INTERNAL_ERROR
+}
