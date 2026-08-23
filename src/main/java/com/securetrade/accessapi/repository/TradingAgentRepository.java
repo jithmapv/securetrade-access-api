8dd14138f0642b4878a98536f600e176aee122af
jithmapv <jithma.hpv@gmail.com>
2026-08-23 13:22:57 +0530

    feat: add method to find TradingAgentEntity by user username with EntityGraph

--- src/main/java/com/securetrade/accessapi/repository/TradingAgentRepository.java
+++ src/main/java/com/securetrade/accessapi/repository/TradingAgentRepository.java
@@ -1,6 +1,7 @@
 package com.securetrade.accessapi.repository;
 
 import com.securetrade.accessapi.entity.TradingAgentEntity;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -11,6 +12,9 @@ public interface TradingAgentRepository extends JpaRepository<TradingAgentEntity
     Optional<TradingAgentEntity> findByUserId(UUID userId);
 
     Optional<TradingAgentEntity> findByAgentCode(String agentCode);
+
+    @EntityGraph(attributePaths = "user")
+    Optional<TradingAgentEntity> findByUserUsername(String username);
 
     boolean existsByAgentCode(String agentCode);
 }
